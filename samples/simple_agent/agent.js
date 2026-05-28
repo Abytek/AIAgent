7f4ce7ea666413904116a649ce45bc96e46644e3
n-c0d3r <ncoderft3t@outlook.com>
2026-05-29 02:05:41 +0700

    update

--- samples/simple_agent/agent.js
+++ samples/simple_agent/agent.js
@@ -4,6 +4,6 @@ const { createAgent } = require("abytek-aiagent");
 const agent = createAgent();
 agent.message({
     role: "user",
-    content: "Hello"
+    content: "Analyze the project at C:/Abytek/Abytek"
 });
 agent.run();
--- tools/powershell/index.js
+++ tools/powershell/index.js
@@ -2,61 +2,67 @@ const { tool } = require("@langchain/core/tools");
 const { z } = require("zod");
 const { spawn } = require("child_process");
 
+async function runPowershell(agent, { script, cwd })
+{
+    return await new Promise((resolve, reject) => {
+
+        const child = spawn(
+            "powershell",
+            [
+                "-NoProfile",
+                "-ExecutionPolicy",
+                "Bypass",
+                "-Command",
+                script,
+            ],
+            {
+                cwd: cwd || agent.path,
+                shell: false,
+            }
+        );
+
+        let stdout = "";
+        let stderr = "";
+
+        child.stdout.on("data", data => {
+            stdout += data.toString();
+        });
+
+        child.stderr.on("data", data => {
+            stderr += data.toString();
+        });
+
+        child.on("error", reject);
+
+        child.on("close", code => {
+
+            resolve(
+                JSON.stringify(
+                    {
+                        success: code === 0,
+                        exitCode: code,
+                        stdout: stdout.trim(),
+                        stderr: stderr.trim(),
+                    },
+                    null,
+                    2
+                )
+            );
+        });
+    });
+}
+
+const MAX_POWERSHELL_TOOL_OUTPUT = 1000;
+
 module.exports = agent => {
     agent.tool(
         tool(
-            async ({
-                script,
-                cwd,
-            }) => {
+            async (args) => {
                 try
                 {
-                    return await new Promise((resolve, reject) => {
-
-                        const child = spawn(
-                            "powershell",
-                            [
-                                "-NoProfile",
-                                "-ExecutionPolicy",
-                                "Bypass",
-                                "-Command",
-                                script,
-                            ],
-                            {
-                                cwd: cwd || agent.path,
-                                shell: false,
-                            }
-                        );
-
-                        let stdout = "";
-                        let stderr = "";
-
-                        child.stdout.on("data", data => {
-                            stdout += data.toString();
-                        });
-
-                        child.stderr.on("data", data => {
-                            stderr += data.toString();
-                        });
-
-                        child.on("error", reject);
-
-                        child.on("close", code => {
-
-                            resolve(
-                                JSON.stringify(
-                                    {
-                                        success: code === 0,
-                                        exitCode: code,
-                                        stdout: stdout.trim(),
-                                        stderr: stderr.trim(),
-                                    },
-                                    null,
-                                    2
-                                )
-                            );
-                        });
-                    });
+                    let consoleOutput = await runPowershell(agent, args);
+                    consoleOutput = consoleOutput.substr(0, Math.min(consoleOutput.length, MAX_POWERSHELL_TOOL_OUTPUT));
+                    return consoleOutput;
                 }
                 catch (error)
                 {
@@ -70,7 +76,7 @@ module.exports = agent => {
             {
                 name: "system.powershell_sync",
                 description:
-                    "Execute a synchornized PowerShell script and return stdout/stderr. Note that this would block the AI agent",
+                    `Execute a synchornized PowerShell script and return stdout/stderr. Note that this would block the AI agent and return max of ${MAX_POWERSHELL_TOOL_OUTPUT} console output characters`,
 
                 schema: z.object({
                     script: z
